9a6f82421addf90ee95ede7be480f14fcd0ff9e9
Juan Vitor Lobo de Freitas <juanvitor2001@outlook.com>
2021-07-11 14:25:13 -0300

    Created Home

---
+++ pages/home.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function First() {
+    return (
+        <Head>
+            <title>Juan Vitor Freitas</title>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+    )
+}
